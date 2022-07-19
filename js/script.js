94ced0ca06d21fcfede94576abd0d2010f367b27
Alex <rinoket@ukr.net>
2022-07-19 12:16:16 +0300

    add script

--- js/script.js
+++ js/script.js
@@ -1,28 +1,5 @@
 
 "use strict";
 
-// alert('Helloщщщщ');
 
- const result = confirm('are you here?');
- console.log(result);
-
-// const answer = +prompt("Vam est 18?","15");
-// console.log(answer+1);
-// console.log(typeof(answer));
-
-// const answers = [];
-
-// answers[0] = prompt("Як ваше імя?");
-// answers[1] = prompt("Як ваша фалімія?");
-// answers[2] = prompt("Скільки вам років?");
-
-// document.write(answers);
-
-// const category = 'toys';
-
-// console.log(`http://Football.ua/${category}/5`);
-
-// const user = 'kuku';
-
-// alert(`Hello ${user}`);
 
